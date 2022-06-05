dcfb8796d30bfe43ba170d082ff0c9747302f863
Ivan Rener <renerivan@outlook.com>
2022-06-05 18:40:28 +0200

    Sign in form has been added

--- src/app/components/authorization/sign-in/sign-in.component.ts
+++ src/app/components/authorization/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -12,4 +13,24 @@ export class SignInComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  repeatPassword = new FormControl('', [Validators.required]);
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  getRepeatPasswordErrorMessage() {
+    return this.repeatPassword.hasError('required') ? 'You must enter a value' : '';
+  }
 }
